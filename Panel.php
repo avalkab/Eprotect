8e108e3410cd2a52ab130a719037c611a62eabda
erhan sönmez <erhan.sonmez@hotmail.com.tr>
2015-08-21 16:29:03 +0300

    first version

---
+++ Panel.php
@@ -0,0 +1,93 @@
+<?php
+
+/*
+	======== USAGE ========
+	
+	Example Url : [controller] / [method] / [action] - admin/news/edit
+	
+	---------------------------------------------------------------------------------------------------------
+
+	require_once( 'Eprotect.php' );
+
+	class Admin
+	{
+		public $eprotect;
+
+		function __construct()
+		{
+			$this->eprotect = new Eprotect( array( 'news' ) );
+
+			$this->eprotect->protectMe( [method], [action] ); // first param is [your dynamic page name], second param [your dynamic page action]
+		}
+
+		public function news()
+		{
+			echo ' News List';
+		}
+	}
+	
+	Thats all finished!
+	You are no longer protected.
+
+	---------------------------------------------------------------------------------------------------------
+*/
+
+class Eprotect
+{
+
+	public $pages = array();
+	public $login_type = 1;
+
+	function __construct( Array $args = null )
+	{
+		$this->pages = $args;
+	}
+
+	public function validPages( $page = null )
+	{
+		return (in_array( $page, $this->pages ) != true) ? false : true;
+	}
+
+	public function validUsers()
+	{
+		return (isset($_SESSION['user']) != 1 || $_SESSION['user']['type_id'] != $this->login_type) ? false : true;
+	}
+
+	public function validUserPerms( $action = null )
+	{
+		return (in_array( $action,  $_SESSION['user']['permissions'] ) != true) ? false : true;
+	}
+
+	public function protectMe( $page = null, $action = null )
+	{
+		try
+		{
+			if ( $this->validPages($page) !== true )
+			{
+				throw new Exception( 'Invalid page' );
+			}
+
+			if ( $this->validUsers() !== true )
+			{
+				throw new Exception( 'Invalid User' );
+			}
+
+			if ( isset($action) )
+			{
+				if ( $this->validUserPerms($action) !== true )
+				{
+					throw new Exception( 'Invalid proccess' );
+				}
+			}
+
+			return true;
+		} 
+		catch ( Exception $e )
+		{
+			echo $e->getMessage();
+			exit;
+		}
+	}
+}
+
+?>
